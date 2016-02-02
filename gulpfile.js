320206e3d0034f68b4198f929a1d66431f01fe80
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2016-02-01 21:06:51 -0800

    build artifacts: rename build dirs to dist

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
 
 gulp.task('clean', function(cb) {
-    del(['lib', 'build', 'test/build']);
+    del(['lib', 'dist', 'test/dist']);
 });
 
 gulp.task('lib', function() {
@@ -27,7 +27,7 @@ gulp.task('lib', function() {
 gulp.task('styles', function() {
     gulp.src('styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build/'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function() {
@@ -41,13 +41,13 @@ gulp.task('browserify-example', ['lib'], function() {
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
         // Start piping stream to tasks!
-        .pipe(gulp.dest('./examples/build/'));
+        .pipe(gulp.dest('./examples/dist/'));
 });
 
 gulp.task('example-serve', ['browserify-example', 'styles', 'watch'], function() {
     connect.server({
         port: 8888,
-        root: ['examples', 'build', 'node_modules']
+        root: ['examples', 'dist', 'node_modules']
     });
 });
 
